c2da70071fb600afb1532b363bca3075c64b828d
Omri H <76548146+omrih4@users.noreply.github.com>
2025-05-11 19:26:19 +0300

    tokyo + logging

--- yaps/src/main.rs
+++ yaps/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use log::info;
+use simplelog::*;
+
+#[tokio::main]
+async fn main() {
+    TermLogger::init(
+        LevelFilter::Info,
+        Config::default(),
+        TerminalMode::Mixed,
+        ColorChoice::Auto,
+    );
+
+    info!("starting up");
 }
